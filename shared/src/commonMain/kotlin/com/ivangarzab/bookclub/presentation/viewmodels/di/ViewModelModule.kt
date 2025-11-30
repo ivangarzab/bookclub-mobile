b23f4eca9a3e5c02a5f5dfdd45f11b073f690ec6
ivangarzab <ivangb6@gmail.com>
2025-11-30 12:38:23 -0800

    feat: Create MeViewModel, -State and add into the Koin's viewModelModule

--- shared/src/commonMain/kotlin/com/ivangarzab/bookclub/presentation/viewmodels/di/ViewModelModule.kt
+++ shared/src/commonMain/kotlin/com/ivangarzab/bookclub/presentation/viewmodels/di/ViewModelModule.kt
@@ -1,9 +1,11 @@
 package com.ivangarzab.bookclub.presentation.viewmodels.di
 
 import com.ivangarzab.bookclub.presentation.viewmodels.club.ClubDetailsViewModel
+import com.ivangarzab.bookclub.presentation.viewmodels.member.MeViewModel
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     factoryOf(::ClubDetailsViewModel)
+    factoryOf(::MeViewModel)
 }
