28607637bf45505ec39f20ab286c253625b7ead0
ivangarzab <ivangb6@gmail.com>
2025-12-26 12:41:16 -0800

    feat(auth): Implement SecureStorage for Android

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -59,6 +59,7 @@ kotlin {
             implementation(libs.ktor.client.android)
             implementation(libs.koin.android)
             implementation(libs.koin.compose)
+            implementation(libs.androidx.security.crypto)
         }
         iosMain.dependencies {
             implementation(libs.ktor.client.darwin)
