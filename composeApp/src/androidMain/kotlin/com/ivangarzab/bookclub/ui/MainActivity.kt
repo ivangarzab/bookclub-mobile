4fc3ec0d5507bba3e181089fac6504a742cdc64f
ivangarzab <ivangb6@gmail.com>
2025-12-26 22:11:54 -0800

    feat(auth): Create LoginScreen and implement into the MainActivity

--- composeApp/src/androidMain/kotlin/com/ivangarzab/bookclub/ui/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/ivangarzab/bookclub/ui/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -12,6 +14,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.ivangarzab.bookclub.theme.KluvsTheme
+import com.ivangarzab.bookclub.ui.login.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +24,9 @@ class MainActivity : ComponentActivity() {
             KluvsTheme {
                 val navController = rememberNavController()
                 MainNavHost(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = MaterialTheme.colorScheme.background),
                     navController = navController
                 )
             }
@@ -37,8 +42,29 @@ fun MainNavHost(
     NavHost(
         modifier = modifier,
         navController = navController,
-        startDestination = NavDestinations.MAIN
+        startDestination = NavDestinations.LOGIN
     ) {
+        composable(NavDestinations.LOGIN) {
+            LoginScreen(
+                onNavigateToSignUp = {
+                    navController.navigate(NavDestinations.SIGNUP)
+                },
+                onNavigateToForgotPassword = {
+                    navController.navigate(NavDestinations.FORGOT_PASSWORD)
+                },
+                onNavigateToMain = {
+                    navController.navigate(NavDestinations.MAIN) {
+                        popUpTo(NavDestinations.LOGIN) { inclusive = true }
+                    }
+                },
+            )
+        }
+        composable(NavDestinations.SIGNUP) {
+            //TODO
+        }
+        composable(NavDestinations.FORGOT_PASSWORD) {
+            //TODO
+        }
         composable(NavDestinations.MAIN) {
             MainScreen()
         }
@@ -46,5 +72,8 @@ fun MainNavHost(
 }
 
 object NavDestinations {
+    const val LOGIN = "login"
+    const val SIGNUP = "signup"
+    const val FORGOT_PASSWORD = "forgot_password"
     const val MAIN = "main"
 }
---
+++ composeApp/src/androidMain/kotlin/com/ivangarzab/bookclub/ui/login/LoginScreen.kt
@@ -0,0 +1,318 @@
+package com.ivangarzab.bookclub.ui.login
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ivangarzab.bookclub.presentation.viewmodels.auth.AuthState
+import com.ivangarzab.bookclub.presentation.viewmodels.auth.AuthViewModel
+import com.ivangarzab.bookclub.presentation.viewmodels.auth.LoginNavigation
+import com.ivangarzab.bookclub.presentation.viewmodels.auth.OAuthProvider
+import com.ivangarzab.bookclub.theme.KluvsTheme
+import com.ivangarzab.bookclub.ui.components.LoadingScreen
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun LoginScreen(
+    modifier: Modifier = Modifier,
+    viewModel: AuthViewModel = koinViewModel(),
+    onNavigateToSignUp: () -> Unit,
+    onNavigateToForgotPassword: () -> Unit,
+    onNavigateToMain: () -> Unit,
+) {
+    val state by viewModel.state.collectAsState()
+
+    when (state) {
+        is AuthState.Loading -> LoadingScreen()
+        is AuthState.Authenticated -> LaunchedEffect(Unit) {
+            onNavigateToMain()
+        }
+        is AuthState.Unauthenticated,
+        is AuthState.Error -> {
+            LoginScreenContent(
+                modifier = modifier,
+                errorMessage = (state as? AuthState.Error)?.message,
+                onEmailSignIn = viewModel::signIn,
+                onOAuthSignIn = { provider ->
+                    // TODO: Implement provider login
+                },
+                onNavigate = {
+                    when (it) {
+                        LoginNavigation.SignUp -> onNavigateToSignUp()
+                        else -> onNavigateToForgotPassword()
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun LoginScreenContent(
+    modifier: Modifier = Modifier,
+    errorMessage: String? = null,
+    onEmailSignIn: (String, String) -> Unit,
+    onOAuthSignIn: (OAuthProvider) -> Unit,
+    onNavigate: (LoginNavigation) -> Unit,
+) {
+    var emailField by remember { mutableStateOf("") }
+    var passwordField by remember { mutableStateOf("") }
+
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    LaunchedEffect(errorMessage) {
+        errorMessage?.let { message ->
+            snackbarHostState.showSnackbar(
+                message = message,
+                duration = SnackbarDuration.Short,
+            )
+        }
+    }
+
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = {
+            SnackbarHost(snackbarHostState) { data ->
+                Snackbar(
+                    snackbarData = data,
+                    containerColor = MaterialTheme.colorScheme.errorContainer,
+                    contentColor = MaterialTheme.colorScheme.onErrorContainer,
+                    actionColor = MaterialTheme.colorScheme.error
+                )
+            }
+        },
+    ) { paddingValues ->
+        Column(
+            modifier = modifier
+                .padding(paddingValues)
+                .padding(16.dp),
+        ) {
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                text = "Welcome to your Kluvs",
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                text = "Sign in to your account",
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                style = MaterialTheme.typography.bodyLarge,
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button( //TODO: Add icon
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { onOAuthSignIn(OAuthProvider.DISCORD) }
+            ) {
+                Text(
+                    text = "Continue with Discord",
+                    color = MaterialTheme.colorScheme.background
+                )
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Button( //TODO: Add icon
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { onOAuthSignIn(OAuthProvider.GOOGLE) }
+            ) {
+                Text(
+                    text = "Continue with Google",
+                    color = MaterialTheme.colorScheme.background
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextDivider(text = "or continue with email")
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = emailField,
+                onValueChange = { emailField = it },
+                singleLine = true,
+                label = { Text("Email") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Email, //TODO: Replace with custom icon
+                        contentDescription = ""
+                    )
+                },
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = passwordField,
+                onValueChange = { passwordField = it },
+                singleLine = true,
+                label = { Text("Password") },
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password, //TODO: Replace with custom icon
+                    imeAction = ImeAction.Go
+                ),
+                keyboardActions = KeyboardActions(
+                    onGo = { onEmailSignIn(emailField, passwordField)}
+                ),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Lock,
+                        contentDescription = ""
+                    )
+                },
+            )
+
+            TextButton (
+                modifier = Modifier
+                    .align(Alignment.End),
+                onClick = { onNavigate(LoginNavigation.ForgetPassword) },
+                content = {
+                    Text(
+                        text = "Forgot password?",
+                        textAlign = TextAlign.Right,
+                        color = MaterialTheme.colorScheme.primary,
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                },
+            )
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { onEmailSignIn(emailField, passwordField) }
+            ) {
+                Text(
+                    text = "Sign In",
+                    color = MaterialTheme.colorScheme.background
+                )
+            }
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Don't have an account?",
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    modifier = Modifier
+                        .clickable(
+                            onClick = { onNavigate(LoginNavigation.SignUp) }
+                        ),
+                    text = "Sign up",
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun TextDivider(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color = Color.Gray,
+    thickness: Dp = 1.dp
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        HorizontalDivider(
+            modifier = Modifier.weight(1f),
+            color = color,
+            thickness = thickness
+        )
+
+        Text(
+            text = text,
+            color = color,
+            fontSize = 14.sp,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        HorizontalDivider(
+            modifier = Modifier.weight(1f),
+            color = color,
+            thickness = thickness
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun Preview_LoginScreen() = KluvsTheme {
+    LoginScreenContent(
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.surface)
+            .fillMaxSize(),
+        onOAuthSignIn = { _ -> },
+        onEmailSignIn = { _, _ -> },
+        onNavigate = { _ -> },
+    )
+}
