20eea45c79915a45330a1895aae9059fb5d5b9e5
ivangarzab <ivangb6@gmail.com>
2025-12-26 20:53:41 -0800

    feat(auth): Create LoginNavigation & OAuthProvider for login flow

---
+++ shared/src/commonMain/kotlin/com/ivangarzab/bookclub/presentation/viewmodels/auth/LoginNavigation.kt
@@ -0,0 +1,10 @@
+package com.ivangarzab.bookclub.presentation.viewmodels.auth
+
+/**
+ * The purpose of this class is to contain all of the different navigation options
+ * from the Login screen.
+ */
+sealed class LoginNavigation {
+    object SignUp: LoginNavigation()
+    object ForgetPassword: LoginNavigation()
+}
